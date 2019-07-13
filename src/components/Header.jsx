7b3a279b6076773f89c076598419135e78dd011c
camille villard <hello@camillevillard.com>
2019-07-12 23:58:21 -0400

    login

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import logo from '../images/mini-logo.svg';
+import menu from "../images/menu.svg"
 
 class Header extends React.Component {
   render() {
@@ -7,19 +8,14 @@ class Header extends React.Component {
       <header className="main-header">
 
       <div className="leftcolum logo">
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a href="/">
           <img src={logo} className="fuzzyfood-logo" alt="logo" />
         </a>
       </div>
 
       <div className="right-column navbar">
 
-      menu
+        <img src={menu} alt=""/>
 
       </div>
 
